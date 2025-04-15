5077f1e57c88df686edadca9510956ba3acae714
Natalie Dorshimer <dorshimer.natalie@gmail.com>
2025-04-15 09:16:03 -0400

    add helper window

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import React from "react";
 import {UpdatingHeatmap} from "@/app/ui/spectrogram/canvas/UpdatingHeatmap";
 import {Controls} from "@/app/ui/spectrogram/controls/Controls";
 import {AnalyzeRecording} from "@/app/ui/AnalyzeRecording";
-import {AdvicePanel} from "@/app/ui/FormantAdviceWindow";
 
 
 const Spectrogram = () => {
@@ -37,7 +36,6 @@ function SpecPlus() {
                     <Spectrogram/>
                 </div>
             </header>
-            <AdvicePanel/>
             <div className="p-5 md:px-2 gap-3 flex flex-col md:flex-row items-start"> {/* Default to col, row on md+ */}
                 <div className="flex-1 w-full"> {/* Ensure children take full width in column layout */}
                     <Controls/>
--- app/ui/FormantAdviceWindow.tsx
+++ app/ui/FormantAdviceWindow.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from "react";
+import React, {ComponentProps, useState} from "react";
 import {A} from "@/app/ui/A";
 import {HelperText, HelperTextExpanded} from "@/app/ui/FormantAnalysis";
+import clsx from "clsx";
 
 interface FormantAdviceWindowProps {
     formant: string
@@ -45,6 +46,27 @@ const FormantAdviceWindow: React.FC<FormantAdviceWindowProps> = ({onClose, child
     </div>
 }
 
+interface WindowProps {
+    onClose: () => void;
+    children: React.ReactNode;
+    header: string;
+}
+
+const Window: React.FC<WindowProps> = ({onClose, children, header}) => {
+    return <div
+        className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-zinc-900 text-white p-6 rounded-md shadow-lg z-10 text-center">
+        <h2 className="text-xl font-semibold mb-4">{header}</h2>
+        <div className="mb-4">
+            {children}
+        </div>
+        <button onClick={onClose}
+                className="bg-zinc-600 hover:bg-zinc-500 text-white py-2 px-4 rounded-md focus:outline-none mt-2">
+            Close
+        </button>
+    </div>
+}
+
+
 interface FormantAdviceButton {
     formant: string;
     setFormant: (formant: string) => void;
@@ -56,6 +78,8 @@ export const AdvicePanel = () => {
     const onWindowClose = () => setFormant(null);
 
     return <div className=" flex gap-5 justify-center">
+
+        <HelperButton/>
         {
             formants.map((formant, index) =>
                 < FormantAdviceButton
@@ -71,10 +95,37 @@ export const AdvicePanel = () => {
     </div>
 }
 
+
+const StandardButton = ({...props}: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) => {
+    const finalClassName = clsx("focus:outline-none bg-zinc-600 hover:bg-zinc-500 text-white rounded-md p-2 text-center sm:w-20 w-16 font-semibold", props.className);
+    return <button {...props} className={finalClassName}>
+    </button>
+}
+
 export const FormantAdviceButton: React.FC<FormantAdviceButton> = ({setFormant, formant}) => {
     return <button onClick={() => setFormant(formant)} className="focus:outline-none">
         <div className="bg-zinc-600 hover:bg-zinc-500 text-white rounded-md p-2 text-center sm:w-24 w-20">
             <div className="font-semibold">{formant}</div>
         </div>
     </button>
+}
+
+export const HelperButton =() => {
+    const [isOpen, setOpen] = useState(false);
+
+    return <>
+        <StandardButton className='sm:w-16 w-16' onClick={() => setOpen(true)}>?</StandardButton>
+        {isOpen ? <Window header='How to Use' onClose={() => setOpen(false)}>
+            <p className={'text-left'}>
+                Trans Voice Trainer helps you train the <b>formants</b> of your voice. You can think of your voice as a bunch of different frequencies layered on top of each other. The <b>formants</b> are the frequencies that are most prominent, or strong, in your voice.
+                <br/><br/>
+                Each formant has different physical characteristics associated with it. You can click on the result of each formant in the analysis window to get advice on you can physically modify your voice to achieve the desired formant.
+                <br/><br/>
+                <li><b>F0</b> is pitch.</li>
+                <li><b>F1</b> is controlled by the throat length. This is the most important formant for feminization.</li>
+                <li><b>F2</b> is controlled by the mouth cavity size and lip shape</li>
+                <li><b>F3</b> is controlled by lip shape</li>
+            </p>
+        </Window> : <></>}
+    </>
 }
