7e244864ad444ebf9acb7ed5cb939771a2760dde
Natalie Dorshimer <dorshimer.natalie@gmail.com>
2025-04-17 19:22:58 -0400

    Clean up some code

--- app/ui/spectrogram/controls/StartStopButton.tsx
+++ app/ui/spectrogram/controls/StartStopButton.tsx
@@ -1,4 +1,4 @@
-import React, {DetailedHTMLProps, useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import clsx from "clsx";
 
 // TODO: This is a mess, fix this in the future
@@ -8,16 +8,12 @@ export const StartStopButton: React.FC<StartStopButtonParams> = (params) => {
     const buttonRef = useRef<HTMLButtonElement>(null);
 
     const handleKeyDown: EventListenerOrEventListenerObject = (event: Event) => {
-        // Check if the pressed key is Enter or Space
         if ((event as KeyboardEvent).key === ' ') {
-            // Prevent the default action of Enter/Space on some elements
             event.preventDefault();
-            // Programmatically trigger the button's onClick handler
             onClick();
         }
     };
 
-    // Attach the event listener to the component containing the button
     useEffect(() => {
         const parentElement = buttonRef.current ? buttonRef.current.parentElement : document; // Or document if no parent
 
@@ -134,44 +130,6 @@ export const StandardSpectrogramButton: React.FC<React.ButtonHTMLAttributes<HTML
     ></button>
 }
 
-export const OnOffButton: React.FC<OnOffButtonParams> = (params) => {
-    const on = params.on || false;
-    const onClick = () => params.setOn(!on);
-
-    const colorsStarted = 'bg-cyan-700 hover:bg-blue-400 focus-visible:outline-blue-500 active:bg-blue-600'
-    const colorsStopped = 'bg-red-700 hover:bg-red-400 focus-visible:outline-red-500 active:bg-red-600';
-
-    const className = clsx(
-        on ? colorsStarted : colorsStopped,
-        'flex',
-        'h-10',
-        'items-center',
-        'rounded-lg',
-        'px-4',
-        'text-sm',
-        'font-medium',
-        'text-white',
-        'transition-colors',
-        'focus-visible:outline',
-        'focus-visible:outline-2',
-        'focus-visible:outline-offset-2',
-        'aria-disabled:cursor-not-allowed',
-        'aria-disabled:opacity-50'
-    );
-    return <div>
-        <button
-            onClick={onClick}
-            className={className}
-        >
-            {on ? params.onText : params.offText}
-        </button>
-    </div>
-}
-
-type NewState = [action: () => void, state: ButtonState]
-type NewStateMapping = {
-    [key in ButtonState]: NewState;
-};
 
 interface ButtonState {
     onText: string;
@@ -182,12 +140,4 @@ interface StartStopButtonParams {
     onStart: () => void;
     onStop?: () => void;
     buttonState?: ButtonState;
-}
-
-interface OnOffButtonParams {
-    onText: string,
-    offText: string,
-    setOn: (state: boolean) => void,
-    on?: boolean,
-    keyControl?: string
 }
